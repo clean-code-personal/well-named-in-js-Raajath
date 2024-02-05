2953de07a1c053ad5f2ec360b5971dffb40c5b78
Rajath .N <89772947+Raajath@users.noreply.github.com>
2024-02-05 13:12:51 +0530

    Split file
    Created separate file for storing pair

---
+++ app/colorsDS.js
@@ -0,0 +1,17 @@
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+function ColorPair(){
+           this.majorColor;
+           this.minorColor;
+}
+
+ColorPair.prototype.toString=function(){
+	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
+}
+
+module.exports={MajorColorNames,MinorColorNames,ColorPair};
