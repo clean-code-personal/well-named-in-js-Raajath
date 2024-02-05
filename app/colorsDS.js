9c9f3b8156a30c3c510d77f5c933d7aef6345833
Rajath .N <89772947+Raajath@users.noreply.github.com>
2024-02-05 14:59:51 +0530

    Added reference Manual here

--- app/colorsDS.js
+++ app/colorsDS.js
@@ -13,5 +13,18 @@ function ColorPair(){
 ColorPair.prototype.toString=function(){
 	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
 }
+   function getReferenceManual() {
+        let manual = "Color Coding Reference Manual:\n\n";
+      
+        for (let majorIndex = 0; majorIndex < MajorColorNames.length; majorIndex++) {
+            for (let minorIndex = 0; minorIndex < MinorColorNames.length; minorIndex++) {
+                let pairNumber = (majorIndex * MinorColorNames.length) + (minorIndex + 1);
+                manual += `${pairNumber}:"Major Color->" ${MajorColorNames[majorIndex]} "Minor color-->" ${MinorColorNames[minorIndex]}\n`;
+            }
+        }
+        return manual;
+      }
+let manual= getReferenceManual();
 
-module.exports={MajorColorNames,MinorColorNames,ColorPair};
+
+module.exports={MajorColorNames,MinorColorNames,ColorPair,manual};
