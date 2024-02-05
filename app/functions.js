7b27b0721a7d50c2e20b0d8fe422f016458b989c
Rajath .N <89772947+Raajath@users.noreply.github.com>
2024-02-05 14:53:25 +0530

    Added Function getReferenceManual

--- app/functions.js
+++ app/functions.js
@@ -46,7 +46,29 @@ function getPairNumberFromColor(pair)
                 throw `Unknown Colors:${pair.toString()}`;
             }
 
+		
         return (majorIndex * MinorColorNames.length) + (minorIndex + 1);
 	 }
+
+
+
+  function getReferenceManual() {
+        let manual = "Reference Manual:\n\n";
+      
+        for (let majorIndex = 0; majorIndex < MajorColorNames.length; majorIndex++) {
+            for (let minorIndex = 0; minorIndex < MinorColorNames.length; minorIndex++) {
+                let pairNumber = (majorIndex * MinorColorNames.length) + (minorIndex + 1);
+                let pair = new ColorPair();
+             
+                manual += `${pairNumber}:"Major Color->" ${MajorColorNames[majorIndex]} "Minor color-->" ${MinorColorNames[minorIndex]}\n`;
+            }
+        }
+        return manual;
+      }
 	 
-     module.exports = { getColorFromPairNumber, getPairNumberFromColor };
+     module.exports = { getColorFromPairNumber, getPairNumberFromColor, getReferenceManual };
+
+
+
+
+
